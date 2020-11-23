dd5b999ec80006de4ba616851b80ce03001f4893
maitrux <60437320+maitrux@users.noreply.github.com>
2020-11-23 17:45:09 +0200

    Toka commit

--- UID/idCardScripts.js
+++ UID/idCardScripts.js
@@ -5,20 +5,100 @@ var nationalities = ["Afghan", "Albanian", "Algerian", "American", "Andorran",
                      "Barbadian", "Belgian", "Belizean", "Beninese",
                      "Bermudian"];
 
+var nonEmptyInputs = ["lastName", "birthDate", "idNumber", "picture"];
+
+var nonEmptySelects = ["gender", "nationality"];
+
 $(document).ready(function() {
   initialize();
+  validateUserInputs();
+  resetForm();
+
+  
+  $('.input').on('input', function()
+  {
+    if($(this).val().length != 0)
+    {
+      $(this).removeClass("is-invalid");
+    }
+  });
+
+  $(".select").on('change', function() {
+    if ($(this).val().length != 0){
+      $(this).removeClass("is-invalid");
+    }
+  });
 });
+
+
 
 function initialize() {
   createNationalityOptions();
 }
 
 function createNationalityOptions() {
-  for(nationality of nationalities) 
+  for(var nationality of nationalities) 
   {
-    console.log(nationality)
-    $('#nationalitySelect').append($('<option>', {
+    $('#nationality').append($('<option>', {
       text: nationality
   }));
   }
+}
+
+function validateUserInputs()
+{
+  $("#btn-create-id-card").click(function(){
+
+    checkEmptyInputs();
+
+    if (!$(".is-invalid")[0])
+    {
+      $("#firstNameConfirmation").text($("#firstName").val());
+      $("#lastNameConfirmation").text($("#lastName").val());
+      $("#birthDateConfirmation").text($("#birthDate").val());
+      $("#genderConfirmation").text($("#gender option:selected").val());
+      $("#nationalityConfirmation").text($("#nationality option:selected").val());
+      $("#idNumberConfirmation").text($("#idNumber").val());
+
+      $('#createIdCardModal').modal('show');
+     
+
+    }
+  });
+}
+
+function checkEmptyInputs()
+{
+  for(var input of nonEmptyInputs)
+  {
+    if($("#" + input).val().length == 0)
+    {
+      $("#" + input).addClass("is-invalid");
+    }
+    else
+    {
+      $("#" + input).removeClass("is-invalid");
+    }
+  }
+
+    for(var select of nonEmptySelects)
+    {
+      if($("#" + select + " option:selected").val().length == 0)
+      {
+        $("#" + select).addClass("is-invalid");
+      }
+      else
+      {
+        $("#" + select).removeClass("is-invalid");
+      }
+    }
+}
+
+function resetForm()
+{
+  $("#clear-form-confimed").click(function(){
+    $('.is-invalid').removeClass('is-invalid');
+    $("#idCard-form").trigger("reset");
+    $('#clearFormModal').modal('toggle');
+  });
 }
